76cb24a5fd46e4183855a03e7743c55b96e4d067
Hammad Usmani <ham.u@live.com>
2025-12-02 06:10:32 +0000

    Refactor and fix bugs that were causing the file paths of the ECC files to error out

--- src/iso.py
+++ src/iso.py
@@ -1,6 +1,6 @@
 import pycdlib
-from PySide6.QtCore import Qt, QRunnable, Slot, QObject, Signal
-
+from PySide6.QtCore import QRunnable, Slot, QObject, Signal
+import traceback
 
 class IsoWorker(QRunnable):
     def __init__(self, output_path, file_list, ecc_dir):
@@ -10,9 +10,73 @@ class IsoWorker(QRunnable):
         self.ecc_dir = ecc_dir
         self.signals = WorkerSignals()
 
+    @Slot()
+    def run(self):
+        '''
+        - For iso_path,
+          In standard ISO interchange level 3, filenames have a maximum of 30 characters, followed by a required
+          dot, followed by an extension, followed by a semicolon and a version. The filename and the extension are
+          both optional, but one or the other must exist. Only uppercase letters, numbers, and underscore are
+          allowed for either the name or extension. If any of these rules are violated, PyCdlib will
+          throw an exception.
+        - If multiple files have the same ISO path, only the most recent one will be written
+        - Rock ridge (rr_name) must be in relative format.
+        '''
+        output_iso = pycdlib.PyCdlib()
+        # https://clalancette.github.io/pycdlib/pycdlib-api.html#PyCdlib-new
+        # Interchange 3 is recommended
+        output_iso.new(interchange_level=3, joliet=3, rock_ridge="1.09", xa=True)
+        # create ECC directory if applicable
+        iso_ecc_dir = '/ECC'
+        if any([file_metadata["ecc_checked"] for file_metadata in self.file_list]):
+            output_iso.add_directory(iso_ecc_dir,
+                                     rr_name=iso_ecc_dir[1:], # must be relative
+                                     joliet_path=iso_ecc_dir)
+        # add files in ISO
+        for file_metadata in self.file_list:
+            standardized = self.standardize_filenames(file_metadata)
+            print(f'\tFile Path: {standardized["file_path"]}')
+            print(f'\t\tSize: {file_metadata["size_str"]}')
+            output_iso.add_file(standardized["file_path"],
+                                iso_path=standardized["iso_path"],
+                                rr_name=standardized["rr_name"],
+                                joliet_path=standardized["joliet_path"])
+            print(f'\t\tECC: {file_metadata["ecc_checked"]}')
+            if file_metadata["ecc_checked"]:
+                # two files added, .txt is the database and .idx is the index (reference pyFileFixity)
+                for ecc_ext in ['.txt', '.txt.idx']:
+                    ecc_file = self.standardize_filenames({
+                        "file_name": file_metadata["file_name"] + ecc_ext,
+                        "directory": self.ecc_dir
+                    })
+                    ecc_iso_path = f'{iso_ecc_dir}{ecc_file["iso_path"]}'
+                    ecc_rr_name = f'{ecc_file["rr_name"]}'
+                    ecc_joliet_path = f'{iso_ecc_dir}{ecc_file["joliet_path"]}'
+                    for param in [ecc_file["file_path"], ecc_iso_path, ecc_rr_name, ecc_joliet_path]:
+                        print(f"\t\t{param}")
+                    try:
+                        output_iso.add_file(ecc_file["file_path"],
+                                            iso_path=ecc_iso_path,
+                                            rr_name=ecc_rr_name,
+                                            joliet_path=ecc_joliet_path)
+                    except Exception as e:
+                        traceback.print_exc()
+                        print(e)
+        print("\tDone adding files to .iso file")
+        def progress_dialog_update(done, total, args):
+            done_ratio = (done / total) * 100
+            self.signals.progress.emit(done_ratio)
+        try:
+            # Write the ISO file with progress updates
+            output_iso.write(self.output_path, progress_cb=progress_dialog_update)
+            output_iso.close()
+            print(f"ISO successfully saved to {self.output_path}")
+        except Exception as e:
+            print(f"Error saving ISO: {e}")
+
     def standardize_filenames(self, file_metadata):
         # sanitize iso name
-        iso_name = file_metadata["file_name"].split(".")[0].upper()
+        iso_name = "".join(file_metadata["file_name"].split("."))[:-1].upper()
         for char in iso_name:
             if (not char.isalnum()) and (char != "_"):
                 iso_name = iso_name.replace(char, "")
@@ -33,65 +97,8 @@ class IsoWorker(QRunnable):
             "rr_name": file_metadata["file_name"]
         }
 
-    @Slot()
-    def run(self):
-        '''
-        For iso_path,
-        In standard ISO interchange level 3, filenames have a maximum of 30 characters, followed by a required
-        dot, followed by an extension, followed by a semicolon and a version. The filename and the extension are
-        both optional, but one or the other must exist. Only uppercase letters, numbers, and underscore are
-        allowed for either the name or extension. If any of these rules are violated, PyCdlib will
-        throw an exception.
-        '''
-        print("Creating .iso file...")
-        output_iso = pycdlib.PyCdlib()
-        # https://clalancette.github.io/pycdlib/pycdlib-api.html#PyCdlib-new
-        # Interchange 3 is recommended
-        output_iso.new(interchange_level=3, joliet=3, rock_ridge="1.09", xa=True)
-        # create ECC directory if applicable
-        iso_ecc_dir = '/ECC'
-        if any([file["ecc_checked"] for file in self.file_list]):
-            output_iso.add_directory(iso_ecc_dir)
-        def add_ecc(ecc_file_name, input_iso, ecc_iso_dir=iso_ecc_dir):
-            ecc_file = self.standardize_filenames({
-                "file_name": ecc_file_name,
-                "directory": self.ecc_dir
-            })
-            input_iso.add_file(ecc_file["file_path"],
-                                iso_path=f'{ecc_iso_dir}{ecc_file["iso_path"]}',
-                                rr_name=f'{ecc_iso_dir}{ecc_file["rr_name"]}',
-                                joliet_path=f'{ecc_iso_dir}{ecc_file["joliet_path"]}')
-            return input_iso
-        # add files in ISO
-        for file_metadata in self.file_list:
-            standardized = self.standardize_filenames(file_metadata)
-            print(f'\tFile Path: {standardized["file_path"]}')
-            print(f'\t\tSize: {file_metadata["size_str"]}')
-            output_iso.add_file(standardized["file_path"],
-                                iso_path=standardized["iso_path"],
-                                rr_name=standardized["rr_name"],
-                                joliet_path=standardized["joliet_path"])
-            print(f'\t\tECC: {file_metadata["ecc_checked"]}')
-            if file_metadata["ecc_checked"]:
-                # two files added, .txt is the database and .idx is the index (reference pyFileFixity)
-                add_ecc(f'{file_metadata["file_name"]}.txt', output_iso)
-                add_ecc(f'{file_metadata["file_name"]}.txt.idx', output_iso)
-        print("\tDone adding files to .iso file")
-
-        def progress_dialog_update(done, total, args):
-            done_ratio = (done / total) * 100
-            self.signals.progress.emit(done_ratio)
-
-        try:
-            # Write the ISO file with progress updates
-            output_iso.write(self.output_path, progress_cb=progress_dialog_update)
-            output_iso.close()
-            print(f"ISO successfully saved to {self.output_path}")
-        except Exception as e:
-            print(f"Error saving ISO: {e}")
-
     def get_ext(self, filename):
-        return "" if len(filename.split(".")) > 0 else filename.split(".")[1]
+        return "" if len(filename.split(".")) > 0 else filename.split(".")[-1]
 
 
 class WorkerSignals(QObject):
